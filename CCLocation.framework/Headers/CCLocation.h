cdb503a14f8f34bdf7de515770670163771a6c69
Ralf Kernchen <ralf@accelogress.com>
2017-03-10 14:18:07 +0000

    added first compiled library files

---
+++ CCLocation.framework/Headers/CCLocation.h
@@ -0,0 +1,19 @@
+//
+//  CCLocation.h
+//  CCLocation
+//
+//  Created by Ralf Kernchen on 16/10/2016.
+//  Copyright © 2016 Crowd Connected. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CCLocation.
+FOUNDATION_EXPORT double CCLocationVersionNumber;
+
+//! Project version string for CCLocation.
+FOUNDATION_EXPORT const unsigned char CCLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCLocation/PublicHeader.h>
+
+#import "CCRequest.h"
---
+++ CCLocation.framework/Headers/CCRequest.h
@@ -0,0 +1,54 @@
+//
+//  CCRequest.h
+//  CCLocation
+//
+//  Created by Ralf Kernchen on 12/04/2014.
+//  Copyright (c) 2014 CrowdConnected. All rights reserved.
+//
+
+#pragma mark - CCRequestDelegate
+
+#import <Foundation/Foundation.h>
+
+@protocol CCRequestDelegate;
+
+@interface CCRequest : NSObject
+
+@property (assign, nonatomic) id <CCRequestDelegate> delegate;
+
+@property (nonatomic, strong) NSDictionary* aliases;
+@property (readonly) NSString* deviceId;
+@property NSDate* startTime;
+
+
++ (id) sharedManager;
+
+- (void) startWithURLString:(NSString *)serverURL apiKey:(NSString*) apiKey;
+- (void) sendMarker:(NSString *)data;
+- (void) sendClientMessage: (NSData* ) clientMessageData queuable:(BOOL)queuable;
+- (void) stop;
+
+- (void) sendWebSocketMessageSwiftBridge:(NSData*) data;
+- (void) setDeviceIdSwiftBridge:(NSString *)deviceId;
+- (void) messageQueuePushSwiftBridge:(NSData*) message;
+- (long) messageQueueCountSwiftBridge;
+- (long) getCachePackageSizeSwiftBridge;
+- (void) setCachePackageSizeSwiftBridge:(NSInteger)cachePackageSize;
+- (NSData*) messageQueuePopSwiftBridge;
+- (void) updateBTSettingsSwiftBridge:(NSNumber*)btleAltBeaconScanTime btleBeaconScanTime:(NSNumber*)btleBeaconScanTime btleAdvertiseTime:(NSNumber*)btleAdvertiseTime idleTime:(NSNumber*)idleTime offTime:(NSNumber*)offTime altBeaconScan:(BOOL)altBeaconScan batchWindow:(NSNumber*)batchWindow state:(NSString*)state;
+- (void) updateGEOSettingsSwiftBridge:(BOOL) network gps:(BOOL)gps cycleOn:(NSInteger)cycleOn cycleOff:(NSInteger)cycleOff interval:(NSInteger)interval minDistance:(NSInteger)minDistance accuracy:(float)accuracy regions:(NSArray*)regions;
+- (void) setiBeaconProximityUUIDsSwiftBridge:(NSArray *)proximitUUIDs;
+- (void) setPingTimerDelaySwiftBridge:(NSInteger)pingTimerDelay;
+- (void) resetPingTimerSwiftBridge;
+- (NSDate*)getStartTimeSwiftBridge;
+
+@end
+
+#pragma mark - CCRequestDelegate
+@protocol CCRequestDelegate <NSObject>;
+
+@required
+- (void) receivedTextMessage:(NSDictionary*) message;
+
+@end
+
